76f63eb1242d84c08993cf409786d415a0f40c6e
kamalyouseff578 <87594710+kamalyouseff578@users.noreply.github.com>
2021-07-18 11:06:57 +0500

    Update CalculatorTest.php

--- calcapp/tests/CalculatorTest.php
+++ calcapp/tests/CalculatorTest.php
@@ -3,12 +3,7 @@ use PHPUnit\Framework\TestCase;
 class CalculatorTest extends TestCase {
 
     /** @test */
-    public function testAdd(){
-        $calculator = new App\Calculator;
-        $result = $calculator->add(21,4);
-        $this->assertEquals(25, $result);
-
-    }
+  
 
     public function testSubtract(){  
         $calculator = new App\Calculator;
@@ -17,19 +12,6 @@ class CalculatorTest extends TestCase {
 
     }
 
-    public function testMultiply(){
-        $calculator = new App\Calculator;
-        $result = $calculator->multiply(21,4);
-        $this->assertEquals(84, $result);
-
-    }
-
-    public function testDivide(){
-        $calculator = new App\Calculator;
-        $result = $calculator->divide(20,4);
-        $this->assertEquals(50,  $result);
-
-    }
 }
 
 
